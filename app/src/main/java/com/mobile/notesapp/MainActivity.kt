c6f542a3477083a4b41b61db60419a48eb3b2142
Galuhika <galuhika02@gmail.com>
2024-09-07 11:04:11 +0700

    action bar

--- app/src/main/java/com/mobile/notesapp/MainActivity.kt
+++ app/src/main/java/com/mobile/notesapp/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.mobile.notesapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mobile.notesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    //menu
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_notes -> {
+                val intentBookmark = Intent(this@MainActivity, AddActivity::class.java)
+                startActivity(intentBookmark)
+                return true
+            }R.id.about -> {
+            val intentTheme = Intent(this@MainActivity, About::class.java)
+            startActivity(intentTheme)
+            return true
+        }
+            else -> return super.onOptionsItemSelected(item)
         }
     }
 }
